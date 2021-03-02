3bcb2221fdff0070e7430bb2ef21f8e806fc1f95
NicolasNewman <nanewman01@gmail.com>
2021-03-02 12:12:35 -0500

    documented IAPI

--- src/api/IAPI.ts
+++ src/api/IAPI.ts
@@ -4,6 +4,10 @@ export default abstract class IAPI<DM> {
 
     private opt: RequestInit;
 
+    /**
+     * @param _name - the name of the integrated service
+     * @param _key - the API key used to access the service
+     */
     constructor(private _name: string, private _key: string) {
         this.opt = {
             method: 'GET',
@@ -27,6 +31,13 @@ export default abstract class IAPI<DM> {
         return this._data;
     }
 
+    /**
+     * Wrapper around JS' build-in fetch function with the following features:
+     * 1) Handles the stringifying and json conversion automatically
+     * 2) Handles the selection of the headers automatically
+     * @param url - the url of the route to request to
+     * @param body - the data to be given through the request
+     */
     async fetch(url, body): Promise<unknown> {
         this.opt.body = JSON.stringify(body);
         const res = await fetch(url, this.opt);
