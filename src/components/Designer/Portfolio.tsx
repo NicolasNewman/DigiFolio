36190bf164cd5ed71b49fc27c2418c90000d43b3
Caleb <calebholland5@gmail.com>
2021-03-11 22:00:00 -0500

    Almost working Drag and Drop Designer Page

--- src/components/Designer/Portfolio.tsx
+++ src/components/Designer/Portfolio.tsx
@@ -2,12 +2,25 @@ import * as React from 'react';
 import { Component } from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Button } from 'antd';
+import { DropTarget, useDrop } from 'react-dnd';
+import PropTypes from 'prop-types';
+import { Connect } from 'react-redux';
+import ItemTypes from '../../constants/types';
+import Widget from './Widget';
 // import DataStore from '../classes/DataStore';
+
+function collect(connect, monitor) {
+    return {
+        connectDropTarget: connect.dropTarget(),
+        hovered: monitor.isOver(),
+        //widget: monitor.getWidget(),
+    };
+}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IProps {}
 
-export default class Portfolio extends Component<IProps> {
+class Portfolio extends Component<IProps> {
     props!: IProps;
 
     constructor(props, history) {
@@ -15,6 +28,26 @@ export default class Portfolio extends Component<IProps> {
     }
 
     render() {
-        return <p>Portfolio!</p>;
+        // const [, drop] = useDrop(() => ({
+        //     accept: ItemTypes.WIDGET,
+        //     collect: (monitor) => ({
+        //         isOver: !!monitor.isOver(),
+        //     }),
+        // }));
+
+        // const { connectDropTarget, hovered } = this.props;
+        const hovered = this.props;
+        const backgroundColor = hovered ? '#F0F02D' : 'white';
+        return (
+            <div className="portfolio">
+                <div
+                    className="portfolio__page"
+                    style={{ background: backgroundColor }}
+                >
+                    <p>This is my page</p>
+                </div>
+            </div>
+        );
     }
 }
+export default DropTarget('widget', {}, collect)(Portfolio);
